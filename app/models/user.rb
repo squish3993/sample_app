f562c517ace06bb9834c1bf89dc8ca7568f521ce
squish3993 <jdlewis@wesleyan.edu>
2019-02-15 15:05:17 +0000

    Implemented advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,13 @@ class User < ApplicationRecord
   
   #Returns true if the given token matches the digest
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  #forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
 
