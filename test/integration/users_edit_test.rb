cf6d0ff4b8d9eda9757f6c674ad98c21d05ef4c2
squish3993 <jdlewis@wesleyan.edu>
2019-02-20 23:36:31 +0000

    Finished user edit, update,  index, and destroy actions

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
    end 
  
    test "unsuccesful edit" do 
+     log_in_as(@user)
      get edit_user_path(@user)
      assert_template 'users/edit'
      patch user_path(@user), params: {user: { name: "",
@@ -17,9 +18,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select "div.alert", text: "The form contains 4 errors."
   end 
   
-  test "succesful edit" do
+  test "succesful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: {user: { name: name,
@@ -32,4 +34,17 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end 
+  
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
