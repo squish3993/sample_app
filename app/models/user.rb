9266ac0d16f4e569f0a9b645400088c7d7118407
squish3993 <jdlewis@wesleyan.edu>
2019-02-22 17:23:01 +0000

    almost finished activation stuff. need to add test and refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   
-  attr_accessor :remember_token
-  
-  before_save {self.email = email.downcase}
+  attr_accessor :remember_token,  :activation_token
+  before_create :create_activation_digest
+  before_save :downcase_email
   
   validates :name, presence: true, length: { maximum: 50 }
   
@@ -33,15 +33,29 @@ class User < ApplicationRecord
   end
   
   #Returns true if the given token matches the digest
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
   
   #forgets a user
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  private
+  
+    #Converts email to all lower case
+    def downcase_email
+      self.email = email.downcase
+    end 
+    
+    #Creates and assigns the activation token and digest
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end 
 end
 
  
