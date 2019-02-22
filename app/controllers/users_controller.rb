3c71a956f1f91514a440f7ab43b94bfcd63958ab
squish3993 <jdlewis@wesleyan.edu>
2019-02-22 18:47:25 +0000

    finished account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
   
   def new
@@ -14,7 +15,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else 
@@ -37,7 +38,7 @@ class UsersController < ApplicationController
   end 
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end 
   
   def destroy
